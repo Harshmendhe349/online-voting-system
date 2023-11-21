5c122f975fa8b7cd79dcec97349274e6858df621
Harshmendhe349 <108999470+Harshmendhe349@users.noreply.github.com>
2023-11-21 13:46:29 +0530

    Update service.js

--- client/src/utils/service.js
+++ client/src/utils/service.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://online-voting-system-blush.vercel.app/";
+const baseUrl = "https://online-voting-system-blush.vercel.app";
 
 const getRequest = async (url) => {
   const response = await fetch(url);
